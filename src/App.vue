887d50f5e97cba1e80b569b3892e666776bcff44
lxyandcyy <lxy_from_swust@foxmail.com>
2020-03-07 23:22:22 +0800

    从摄像头获取base64图片数据

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,11 @@
 <template>
   <div id="app">
-    <button @click="handleVideo">打开摄像头</button>
-    <video id="v" ref="video" :srcObject="this.videoInfo.srcObject"></video>
+    <button @click="openVideo">打开摄像头</button>
+    <button @click="takePhoto">拍照</button>
+    <video id="v" ref="video" style="width: 640px;height: 480px;"></video>
+    <canvas id="canvas" ref="canvas" style="display:none;"></canvas>
+    <br />
+    <img ref="photo" id="photo" alt="photo" />
   </div>
 </template>
 
@@ -9,13 +13,11 @@
 export default {
   data() {
     return {
-      videoInfo: {
-        srcObject: ""
-      }
+      videoPlaying: false //是否开启摄像头
     };
   },
   methods: {
-    handleVideo() {
+    openVideo() {
       let Media = navigator.mediaDevices.getUserMedia({
         audio: false,
         video: {
@@ -25,15 +27,29 @@ export default {
       });
       Media.then(mediaStream => {
         //成功时调用的callback函数
-        this.videoInfo.srcObject = mediaStream;
         let videoObj = this.$refs.video;
-        videoObj.onloadedmetadata = function(e) {
-          video.play();
+        videoObj.srcObject = mediaStream; //设置video的srcObject
+        videoObj.onloadedmetadata = e => {
+          videoObj.play();
+          this.videoPlaying = true;
         };
       }).catch(err => {
         //失败时调用的callback函数
-        console.log("The following error occurred: " + err.name);
+        console.log("The following error occurred: " + err.message);
       });
+    },
+    takePhoto() {
+      if (this.videoPlaying) {
+        let canvas = this.$refs.canvas;
+        let videoObj = this.$refs.video;
+        let photoObj = this.$refs.photo;
+
+        canvas.width = videoObj.videoWidth;
+        canvas.height = videoObj.videoHeight;
+        canvas.getContext("2d").drawImage(videoObj, 0, 0);
+        let dataURL = canvas.toDataURL("image/png"); //canvas转换成base64图片
+        photoObj.src = dataURL; //设置photo的src
+      }
     }
   }
 };
